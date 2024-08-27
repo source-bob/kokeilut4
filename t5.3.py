4468ba35ad6c2c9c749f287e83f031218c9f4ed6
onion <onionscream@gmail.com>
2024-08-27 04:09:05 +0300

    moi

--- t5.3.py
+++ t5.3.py
@@ -1,12 +1,16 @@
-check = list(range(1, 9))
-
 while True:
-    luku = float(input('anna luku: '))
-    counter = 0
-    for point in check:
-        if luku % point == 0 and luku != 1 and point != 1 and luku != point:
-            counter += 1
-    if counter == 0 or luku == 1:
+    luku = int(input('anna luku: '))
+    if luku < 2:
+        print('luku ei ole alkuluku')
+        continue
+
+    is_prime = True
+    for i in range(2, int(luku ** 0.5) + 1):
+        if luku % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
         print('luku on alkuluku')
-    elif counter != 0:
+    else:
         print('luku ei ole alkuluku')
